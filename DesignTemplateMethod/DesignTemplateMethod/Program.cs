a37be44f85f16742a191688caa0549517137abeb
paulolisboa38 <paulolisboa38@gmail.com>
2023-11-25 17:40:59 -0300

    Implement demonstration of daily routines for all worker types in Main

--- DesignTemplateMethod/DesignTemplateMethod/Program.cs
+++ DesignTemplateMethod/DesignTemplateMethod/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace DesignTemplateMethod
+﻿using System;
+
+namespace DesignTemplateMethod
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Worker[] workers = new Worker[]
+            {
+                    new FireFighter(),
+                    new Lumberjack(),
+                    new Postman(),
+                    new Manager()
+            };
+
+            for(int i = 0; i < workers.Length; i++)
+            {
+                Console.WriteLine($"\n Routine of a {workers[i].GetType().Name}:");
+                workers[i].DailyRoutine();
+            }
+
+            Console.ReadKey();
         }
     }
 }
