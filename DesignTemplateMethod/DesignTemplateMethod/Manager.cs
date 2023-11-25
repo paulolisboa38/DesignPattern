7aba0b1dcd89058a617576235044cab283db8b41
paulolisboa38 <paulolisboa38@gmail.com>
2023-11-25 17:18:47 -0300

    Refactor Manager class to extend Worker with overridden methods

--- DesignTemplateMethod/DesignTemplateMethod/Manager.cs
+++ DesignTemplateMethod/DesignTemplateMethod/Manager.cs
@@ -2,44 +2,24 @@
 
 namespace DesignTemplateMethod
 {
-    internal class Manager
+    internal class Manager : Worker
     {
-        public void GetUp()
+        protected override void GoToWork()
         {
-            Console.WriteLine(" Getting out of bed...");
+            Console.WriteLine(" Taking a taxi.");
         }
 
-        public void EatBreakfast()
-        {
-            Console.WriteLine(" Having breakfast...");
-        }
-
-        public void GoToWork()
-        {
-            Console.WriteLine(" Going to work...");
-        }
-
-        public void Work()
+        protected override void Work()
         {
             Console.WriteLine
                 (" Overseeing operations, managing staff, making strategic decisions," +
                 " and coordinating with other departments.");
         }
 
-        public void ReturnToHome()
-        {
-            Console.WriteLine(" Returning home...");
-        }
-
-        public void Relax()
+        protected override void Relax()
         {
             Console.WriteLine(" Playing golf, reading industry-related literature," +
                 "networking at events, or enjoying a quiet evening.");
-        }
-
-        public void Sleep()
-        {
-            Console.WriteLine(" Sleeping...");
         }
     }
 }
