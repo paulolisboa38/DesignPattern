009c571337baa0aa68d77b7c2a5c6b210c69ca0d
Paulo Lisboa <paulolisboa38@gmail.com>
2023-11-25 06:30:41 -0300

    Add Postman class

---
+++ DesingTemplateMethod/DesingTemplateMethod/DesingTemplateMethod/Postman.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DesingTemplateMethod
+{
+    internal class Postman
+    {
+        public void GetUp()
+        {
+            Console.WriteLine(" Getting out of bed...");
+        }
+
+        public void EatBreakfast()
+        {
+            Console.WriteLine(" Having breakfast...");
+        }
+
+        public void GoToWork()
+        {
+            Console.WriteLine(" Going to work...");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine
+                (" Sorting mail, delivering letters and packages, and collecting outgoing mail...");
+        }
+
+        public void ReturnToHome()
+        {
+            Console.WriteLine(" Returning home...");
+        }
+
+        public void Relax()
+        {
+            Console.WriteLine(" Relaxing...");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(" Sleeping...");
+        }
+    }
+}
