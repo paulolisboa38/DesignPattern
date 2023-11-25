bf42b014f460253682e88c588b56a037c7dc412e
Paulo Lisboa <paulolisboa38@gmail.com>
2023-11-25 13:21:48 -0300

    Add Manager class

---
+++ DesingTemplateMethod/DesingTemplateMethod/DesingTemplateMethod/Manager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DesingTemplateMethod
+{
+    internal class Manager
+    {
+        public void GetUp()
+        {
+            Console.WriteLine(" Getting out of bed...");
+        }
+
+        public void EatBreakfast()
+        {
+            Console.WriteLine(" Having breakfast...");
+        }
+
+        public void GoToWork()
+        {
+            Console.WriteLine(" Going to work...");
+        }
+
+        public void Work()
+        {
+            Console.WriteLine
+                (" Overseeing operations, managing staff, making strategic decisions," +
+                " and coordinating with other departments...");
+        }
+
+        public void ReturnToHome()
+        {
+            Console.WriteLine(" Returning home...");
+        }
+
+        public void Relax()
+        {
+            Console.WriteLine(" Relaxing...");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(" Sleeping...");
+        }
+    }
+}
