817f223b929be9ff6b5e60436b3fa784875e1f94
Paulo Lisboa <paulolisboa38@gmail.com>
2023-11-25 15:18:24 -0300

    Add Worker  base class

---
+++ DesingTemplateMethod/DesingTemplateMethod/DesingTemplateMethod/Worker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DesignTemplateMethod
+{
+    internal class Worker
+    {
+        // Template method
+        public void DailyRoutine()
+        {
+            GetUp();
+            EatBreakfast();
+            GoToWork();
+            Work();
+            ReturnToHome();
+            Relax();
+            Sleep();
+        }
+
+        protected void GetUp()
+        {
+            Console.WriteLine(" Getting out of bed...");
+        }
+
+        protected void EatBreakfast()
+        {
+            Console.WriteLine(" Having breakfast...");
+        }
+
+        protected void GoToWork()
+        {
+            Console.WriteLine(" Going to work...");
+        }
+
+        protected void Work()
+        {
+            Console.WriteLine(" Working...");
+        }
+
+        protected void ReturnToHome()
+        {
+            Console.WriteLine(" Returning home...");
+        }
+
+        protected void Relax()
+        {
+            Console.WriteLine(" Relaxing...");
+        }
+
+        protected void Sleep()
+        {
+            Console.WriteLine(" Sleeping...");
+        }
+    }
+}
